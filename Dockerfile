3db7a15b526b8773f35647999236344335b67126
vegura <annadominie@gmail.com>
2023-11-25 22:21:30 +0200

    Set up docker build prod configuration. Fix lint comments
    Add resource image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,23 @@
-FROM node:18
+FROM node:18-alpine
+RUN mkdir -p /app
 WORKDIR /app
 
+#ADD . /app
 COPY public /app/public
 COPY src /app/src
 COPY package.json /app/
+COPY tsconfig.json /app/
 
-RUN npm install
+#COPY . /app
 
-CMD ["npm", "start"]
+#RUN #npm install -g yarn
+#    yarn && \
+#    yarn run build && \
+#    yarn cache clean
+RUN yarn install
+
+RUN npm run build
+RUN yarn global add serve
+
+CMD ["serve", "-s", "build"]
+EXPOSE 8080
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Content} from "./test";
 import {WebAppProvider} from "@vkruglikov/react-telegram-web-app";
+import {Typography} from "@mui/joy";
 
 function App() {
-    const [smoothButtonsTransition, setSmoothButtonsTransition] = useState(false);
+    const [smoothButtonsTransition] = useState(false);
 
     return (
         <WebAppProvider options={{smoothButtonsTransition}}>
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
                     <Content/>
+                    <img src={"/logo1.jpg"} style={{borderRadius: "10%"}} alt={"Evil face"}/>
+                    <Typography level={"h2"} color="neutral" style={{marginTop: "20px"}}>Everything works</Typography>
                 </header>
             </div>
         </WebAppProvider>
